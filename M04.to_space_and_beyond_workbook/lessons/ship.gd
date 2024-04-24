f56b14a54a1689126f224857538de6ed98394f85
Jonng <jonngkh@gmail.com>
2024-04-24 22:45:11 +0800

    right before turning the ship only when moving

--- M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,7 +1,16 @@
 extends Sprite2D
 
-var velocity := Vector2(480, 480)
+var max_speed := 600
+var velocity := Vector2.ZERO
 
 func _process(delta: float) -> void:
+	var direction := Vector2.ZERO
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+
+	velocity = direction * max_speed
 	position += velocity * delta
 	rotation = velocity.angle()
