17e1aa4a595f02a509e19f3163e47db00f68326a
Jonng <jonngkh@gmail.com>
2024-04-28 20:36:20 +0800

    health packs add health

--- M05.loot_it_all_workbook/lessons/ship.gd
+++ M05.loot_it_all_workbook/lessons/ship.gd
@@ -1,9 +1,19 @@
 extends Area2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_polygon_2d: CollisionPolygon2D = $CollisionPolygon2D
+@onready var health_bar: ProgressBar = $UI/HealthBar
+
 
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
 var steering_factor := 3.0
+var health := 10.0
+var heal_amount := 10
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	set_health(health)
 
 
 func _process(delta: float) -> void:
@@ -20,4 +30,14 @@ func _process(delta: float) -> void:
 	position += velocity * delta
 
 	if velocity.length() > 0.0:
-		rotation = velocity.angle()
+		sprite_2d.rotation = velocity.angle()
+		collision_polygon_2d.rotation = velocity.angle()
+
+
+func _on_area_entered(area_that_entered: Area2D) -> void:
+	set_health(health + heal_amount)
+
+
+func set_health(new_health: int) -> void:
+	health = new_health
+	health_bar.value = health
