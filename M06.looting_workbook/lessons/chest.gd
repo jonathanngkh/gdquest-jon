9f271a364682ceaa9b7e53d0b4cb0927f7e15c05
Jonng <jonngkh@gmail.com>
2024-05-06 07:01:00 +0800

    L6P1, @export var possible_items: Array[PackedScene], polar coordinate spawn items in a circle randomly

--- M06.looting_workbook/lessons/chest.gd
+++ M06.looting_workbook/lessons/chest.gd
@@ -3,6 +3,8 @@ extends Area2D
 @onready var canvas_group: CanvasGroup = $CanvasGroup
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@export var possible_items: Array[PackedScene] = [] # if selected in the inspector, those packed scenes become auto pre-loaded
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,6 +17,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+
+func _spawn_random_item() -> void:
+	var loot_item: Area2D = possible_items.pick_random().instantiate()
+	#loot_item.position = Vector2(loot_item.position.x + randi_range(-65, 65), loot_item.position.y + randi_range(-65, 65)) # simple rand distance and angle
+	var starting_angle = 0.0 # 0 radians points to the right
+	var ending_angle = 2.0 * PI # 2π = 360 degrees, so one full circle
+	var random_angle := randf_range(starting_angle, ending_angle)
+	
+	var random_direction := Vector2(1.0, 0.0).rotated(random_angle) # random direction is a unit vector pointing right, rotated at a random angle
+	
+	var min_dist_to_chest = 60
+	var max_dist_to_chest = 120
+	var random_distance := randf_range(min_dist_to_chest, max_dist_to_chest)
+	
+	loot_item.position = random_direction * random_distance
+	add_child(loot_item)
 
 # best for single events as opposed to continuous input streams, as this function is called once every time user presses and input
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
@@ -39,6 +57,13 @@ func open() -> void:
 	animation_player.play("open")
 	input_pickable = false
 		#set_outline_thickness(3.0)
+		
+	if possible_items.is_empty():
+		print("possible items is empty")
+		return
+		
+	for i in range(randi_range(1, 3)):
+		_spawn_random_item()
 
 func _on_mouse_entered() -> void:
 	var tween = create_tween()
