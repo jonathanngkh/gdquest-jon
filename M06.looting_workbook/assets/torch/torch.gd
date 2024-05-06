d96c5c743367afdf7903feafb70a4b790f7de600
Jonng <jonngkh@gmail.com>
2024-05-07 03:13:14 +0800

    Module 6 experimentation with tweens and toggling flames and consuming items

--- M06.looting_workbook/assets/torch/torch.gd
+++ M06.looting_workbook/assets/torch/torch.gd
@@ -1,7 +1,28 @@
-extends Node2D
+extends Area2D
 
 @onready var flame: Sprite2D = $Flame
 
 func _ready() -> void:
 	# This parameter of the shader material gives each flame a slightly different look and randomized animation.
 	flame.material.set("shader_parameter/offset", global_position * 0.1)
+
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+	if event_is_mouse_click:
+		var tween = create_tween()
+		if $Flame.visible:
+			tween.tween_property($Flame, "scale", Vector2(0.0, 0.0), 0.2)
+			tween.finished.connect(toggle_flame_visibility)
+		else:
+			tween.tween_property($Flame, "scale", Vector2(0.28, 0.28), 0.2)
+			toggle_flame_visibility()
+		tween.play()
+
+func toggle_flame_visibility():
+		$Flame.visible = not $Flame.visible
+	
--- M06.looting_workbook/lessons/items/item.gd
+++ M06.looting_workbook/lessons/items/item.gd
@@ -1,8 +1,12 @@
 # Script for pickable items. We don't use inheritance here, just composition: an item is any Area2D scene with a child sprite and this script attached. See gem.tscn and health_pack.tscn.
 extends Area2D
 
+@onready var canvas_group: CanvasGroup = $CanvasGroup
+
 
 func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 	play_floating_animation()
 
 
@@ -11,7 +15,7 @@ func play_floating_animation() -> void:
 	tween.set_loops()
 	tween.set_trans(Tween.TRANS_SINE)
 
-	var sprite_2d := get_node("Sprite2D")
+	var sprite_2d := $CanvasGroup/Sprite2D
 	var position_offset := Vector2(0.0, 4.0)
 	var duration = randf_range(0.8, 1.2)
 	sprite_2d.position = -1.0 * position_offset
@@ -19,3 +23,36 @@ func play_floating_animation() -> void:
 	tween.tween_property(sprite_2d, "position",  -1.0 * position_offset, duration)
 
 
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	var event_is_LMB_clicked: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+	if event_is_LMB_clicked:
+		var tween := create_tween()
+		tween.tween_property(self, "scale", Vector2(0.0, 0.0), 0.08)
+		tween.finished.connect(queue_free)
+		tween.play()
+
+
+func _on_mouse_entered() -> void:
+	print("entered")
+	var tween = create_tween()
+	var from_thickness := 3.0
+	var to_thickness := 6.0
+	var tween_duration := 0.08
+	#if not is_open:
+	tween.tween_method(set_outline_thickness, from_thickness, to_thickness, tween_duration)
+
+
+func _on_mouse_exited() -> void:
+	var tween = create_tween()
+	var from_thickness := 6.0
+	var to_thickness := 3.0
+	var tween_duration := 0.07
+	#if not is_open:
+	tween.tween_method(set_outline_thickness, from_thickness, to_thickness, tween_duration)
+
+func set_outline_thickness(new_thickness: float) -> void:
+	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
