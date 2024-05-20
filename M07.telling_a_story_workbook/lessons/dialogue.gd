2aaa767749abb06c1948802392b8d38de7a8f95f
Jonng <jonngkh@gmail.com>
2024-05-20 20:08:43 +0800

    slide in tween, array[dictionary], expressions combined with text

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -4,25 +4,67 @@ extends Control
 @onready var next_button: Button = %NextButton
 @onready var talking_sound: AudioStreamPlayer = $TalkingSound
 @onready var talking_timer: Timer = $TalkingTimer
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
 
-var dialogue_items: Array[String] = [
-	"Harmonious Hellos!",
-	"I'm Sofia.",
-	"I love scales!",
-	"Let's practice together.",
-	"Do...",
-	"Re...",
-	"Mi...",
-	"Fa...",
-	"So...",
-	"So... f...",
-	"So... fu...",
-	"So... fun!",
+var expressions: Dictionary = {
+	"regular" = preload("res://assets/emotion_regular.png"),
+	"happy" = preload("res://assets/emotion_happy.png"),
+	"angry" = preload("res://assets/extras/emotion_angry.png"),
+	"determined" = preload("res://assets/extras/emotion_determined.png"),
+	"sad" = preload("res://assets/emotion_sad.png"),
+	"euphoric" = preload("res://assets/emotion_euphoric.png"),
+}
+
+var dialogue_items: Array[Dictionary] = [
+	{"text": "Harmonious Hellos!",
+	"expression": expressions["regular"]},
+	
+	{"text": "I'm Sofia.",
+	"expression" : expressions["happy"]},
+	
+	{"text": "I'll be your scales practice coach.",
+	"expression" : expressions["regular"]},
+	
+	{"text": "Some people find practicing scales bothersome...",
+	"expression" : expressions["sad"]},
+	
+	{"text": "But I love scales!",
+	"expression": expressions["happy"]},
+	
+	{"text": "Let's practice together.",
+	"expression": expressions["determined"]},
+	
+	{"text": "Do...",
+	"expression": expressions["regular"]},
+	
+	{"text": "Re...",
+	"expression": expressions["euphoric"]},
+	
+	{"text": "Mi...",
+	"expression": expressions["sad"]},
+	
+	{"text": "Fa...",
+	"expression": expressions["happy"]},
+	
+	{"text": "So...",
+	"expression": expressions["regular"]},
+	
+	{"text": "So... f...",
+	"expression": expressions["angry"]},
+	
+	{"text": "So... fu...",
+	"expression": expressions["determined"]},
+	
+	{"text": "So... fun!",
+	"expression": expressions["happy"]},
 ]
+
 var seconds_per_character := 0.6/18.0
 var current_index: int = 0
-var current_item: String = dialogue_items[current_index]
-var text_appearing_duration: float = current_item.length() * seconds_per_character
+var current_item: Dictionary = dialogue_items[current_index]
+var current_text: String = current_item["text"]
+var text_appearing_duration: float = current_text.length() * seconds_per_character
 var talking_playback_position := 0.0
 
 func _ready() -> void:
@@ -31,10 +73,8 @@ func _ready() -> void:
 	talk()
 	show_text()
 
-
 func talk() -> void:
-	text_appearing_duration = float(current_item.length()) * seconds_per_character
-	print(current_item.length() * seconds_per_character)
+	text_appearing_duration = float(current_text.length()) * seconds_per_character
 	talking_timer.wait_time = text_appearing_duration
 	talking_sound.play(talking_playback_position)
 	talking_timer.start()
@@ -48,44 +88,58 @@ func _on_talking_timer_timeout() -> void:
 func _on_button_pressed() -> void:
 	play_scale()
 	advance_dialogue()
+	slide_in()
 
 
 func show_text() -> void:
 	current_item = dialogue_items[current_index]
-	rich_text_label.text = current_item
-	var dialogue_before_scale_starts_or_last_line: bool = (
-		current_index <= 3 or
-		current_index == dialogue_items.size() -1 
-	)
+	current_text = current_item["text"]
+	rich_text_label.text = current_text
+	expression.texture = current_item["expression"]
 	talk()
-	if dialogue_before_scale_starts_or_last_line:
-		var tween = create_tween()
-		tween.tween_property(rich_text_label, "visible_ratio", 1, text_appearing_duration).from(0.0)
+	var tween = create_tween()
+	tween.tween_property(rich_text_label, "visible_ratio", 1, text_appearing_duration).from(0.0)
 
 
 func advance_dialogue() -> void:
 	if current_index + 1 < dialogue_items.size():
 		current_index += 1
-		current_item = dialogue_items[current_index]
 	show_text()
 
 
+func slide_in() -> void:
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(body, "position:x", 194, 0.5).from(400)
+	
+	tween = create_tween()
+	#tween.set_ease(Tween.EASE_IN)
+	#tween.set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(body, "modulate:a", 1.0, 0.2).from(0.0)
+	
+	tween = create_tween()
+	#tween.set_ease(Tween.EASE_IN_OUT)
+	#tween.set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(body, "scale:y", 0.9, 0.1).from(1.0)
+	tween.tween_property(body, "scale:y", 1.0, 0.1).from(0.9)
+
 func play_scale() -> void:
-	if current_item == "Let's practice together.":
+	if current_text == "Let's practice together.":
 		$NextButton/Do.play()
-	if current_item == "Do...":
+	if current_text == "Do...":
 		$NextButton/Re.play()
-	if current_item == "Re...":
+	if current_text == "Re...":
 		$NextButton/Mi.play()
-	if current_item == "Mi...":
+	if current_text == "Mi...":
 		$NextButton/Fa.play()
-	if current_item == "Fa...":
+	if current_text == "Fa...":
 		$NextButton/So.play()
-	if current_item == "So...":
+	if current_text == "So...":
 		$NextButton/La.play()
-	if current_item == "So... f...":
+	if current_text == "So... f...":
 		$NextButton/Ti.play()
-	if current_item == "So... fu...":
+	if current_text == "So... fu...":
 		$NextButton/Mi.play()
 		$NextButton/So.play()
 		$"NextButton/High Do".play()
